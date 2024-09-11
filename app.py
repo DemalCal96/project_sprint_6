7ea5772763989694b7a1f49269f314a955ba6c3f
DemalCal96 <jossuenavarro@gmail.com>
2024-09-11 01:57:26 -0500

    Update in scatter graph and location of EDA notebook

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ if disp_button: # al hacer clic en el botón
             st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
             
             # crear un gráfico de dispersión
-            fig = px.scatter(car_data, x="odometer")
+            fig = px.scatter(car_data, x="odometer" y="price")
         
             # mostrar un gráfico Plotly interactivo
             st.plotly_chart(fig, use_container_width=True)
